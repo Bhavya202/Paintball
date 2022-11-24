28ed3f47a43309d752bcd81f8303518fc8015551
Bhavya <76608764+Bhavya202@users.noreply.github.com>
2022-11-24 21:08:04 +0530

    Add files via upload

--- shoot.js
+++ shoot.js
@@ -14,13 +14,13 @@ AFRAME.registerComponent("bullets", {
 
         bullet.setAttribute("material", "color", "black");
 
-        var cam = document.querySelector("#camera");
+        var cam = document.querySelector("#camera-rig");
         pos = cam.getAttribute("position");
 
         bullet.setAttribute("position", {
           x: pos.x,
-          y: pos.y,
-          z: pos.z,
+          y: pos.y+1.6,
+          z: pos.z-0.08,
         });
 
         var camera = document.querySelector("#camera").object3D;
@@ -39,6 +39,7 @@ AFRAME.registerComponent("bullets", {
           shape: "sphere",
           mass: "0",
         });
+
         bullet.setAttribute("visible", false);
 
         //add the collide event listener to the bullet
@@ -64,6 +65,7 @@ AFRAME.registerComponent("bullets", {
     var pos = element.getAttribute("position")
     var rotate = elementHit.getAttribute("rotation")
 
+    //set the position, rotation, scale
     paint.setAttribute("position", {
       x: pos.x,
       y: pos.y,
@@ -80,7 +82,9 @@ AFRAME.registerComponent("bullets", {
       z: 2,
     });
 
+    //choose the paint splash image randomly
     var colorNum = parseInt(Math.random() * 8 + 1)
+
     paint.setAttribute("material", {
       opacity: 1,
       transparent: true,
